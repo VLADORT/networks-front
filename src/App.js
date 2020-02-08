b7622d78c6a13f9da9b610de95e100755c308ac6
Vlad <vlad300820001@gmail.com>
2020-02-08 10:05:40 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            networks: []
+        }
+
+    };
+
+    componentDidMount() {
+        fetch('http://localhost:9091/api/node/',
+            {
+                mode: 'cors',
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            })
+            .then((response) => response.json())
+            .then(json => this.setState({networks: json}));
+    }
+
+    render() {
+        let {networks} = this.state;
+        return (
+            <div className="App">
+                <ul>
+                    {networks.map(n=>
+                        <li id={n.id}>
+                          {n.id}
+                        </li>
+                    )}
+                </ul>
+            </div>
+        );
+
+    }
 }
 
 export default App;
